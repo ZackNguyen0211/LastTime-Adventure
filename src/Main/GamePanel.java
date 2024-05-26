a84c6ce3927e9dad2efca95331555913239115f8
Thanh Nguyễn <113710514+ZackNguyen0211@users.noreply.github.com>
2024-05-26 12:52:03 +0700

    update main

---
+++ src/Main/GamePanel.java
@@ -0,0 +1,76 @@
+package Main;
+
+import Entity.Player;
+import Tiles.TileManager;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+    //Screen Settings
+    final int originalTileSize = 16; //16x16 tile
+    final int scale = 3;
+    public final int tileSize = originalTileSize * scale; //48x48 tile
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenCol; //768 pixels
+    public final int screenHeight = tileSize * maxScreenRow; //576 pixels
+
+    //World settings
+    public final int maxWorldCol = 32;
+    public final int maxWorldRow = 24;
+    public final int worldWidth = tileSize * maxWorldCol;
+    public final int worldHeight = tileSize * maxWorldRow;
+
+    int FPS = 60;
+
+    TileManager tileM = new TileManager(this);
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+    public CollisionCheck cChecker = new CollisionCheck(this);
+    public Player player = new Player(this, keyH);
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    @Override
+    public void run(){
+        //"Delta" method for GameLoop
+        //60 FPS
+        double drawInterval = (double) 1000000000 /FPS; //0.01666s
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while(gameThread != null){
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime)/drawInterval;
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;
+            }
+        }
+    }
+    public void update(){
+        player.update();
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        tileM.draw(g2);
+        player.draw(g2);
+        g2.dispose();
+    }
+}
