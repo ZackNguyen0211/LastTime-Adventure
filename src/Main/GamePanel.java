a2a3ec31eac92aa65ac2b8a7637c89394eab10f3
nghianguyen19 <nghianguyen1129@gmail.com>
2024-05-28 14:08:52 +0700

    Update GamePanel.java

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -40,6 +40,7 @@ public class GamePanel extends JPanel implements Runnable {
     public final int titleState = 0;
     public final int playState = 1;
     public final int pauseState = 2;
+    public final int optionsState = 5;
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
