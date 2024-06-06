4318dbc03cec0663d1730003d984478d8450df30
Thanh Nguyễn <113710514+ZackNguyen0211@users.noreply.github.com>
2024-06-06 23:42:18 +0700

    fix bug 1

--- src/Entity/Entity.java
+++ src/Entity/Entity.java
@@ -10,6 +10,7 @@ public class Entity {
     public int worldX, worldY;
     public int speed;
     public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public BufferedImage attackUp1, attackUp2, attackDown1, attackDown2, attackLeft1, attackLeft2, attackRight1, attackRight2;
     public String direction;
     public int spriteCounter = 0;
     public int spriteNum = 1;
@@ -112,4 +113,5 @@ public class Entity {
             g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
         }
     }
+
 }
